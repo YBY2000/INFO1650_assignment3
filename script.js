97c254704750f5f373ca90f3e5dea8d883d94261
Boyuan YE <ye.boy@northeastern.edu>
2023-09-28 00:18:33 -0400

    docs: :memo: All done!

--- script.js
+++ script.js
@@ -18,8 +18,9 @@ var socialMedia = {
 var t = new Title("CONNECT WITH ME!");
 
 
-const detailContent = '<tr class="dropDownTextArea"><td colspan="8">Advisor:<br /><br />Award Details<br />Summer 1-2014(TA)<br />Budget Number: <br />Tuition Number: <br />Comments:<br /><br /><br />Award Status:<br /><br /><br /></td></tr>';
+const detailContent = '<td colspan="8">Advisor:<br /><br />Award Details<br />Summer 1-2014(TA)<br />Budget Number: <br />Tuition Number: <br />Comments:<br /><br /><br />Award Status:<br /><br /><br /></td>';
 let curIndex = 3;
+let selectedNum = 0;
 
 /* 
  * Click the add button to add a recordd
@@ -40,9 +41,10 @@ function addRecord() {
   let tdBudget = document.createElement("td");
   let tdPercentage = document.createElement("td");
   let tdDelete = document.createElement("td");
+  let tdEdit = document.createElement("td");
 
   // insert the basic information
-  tdCheckbox.innerHTML = '<input type="checkbox" onclick="selectRecord(this)"/><br /><br /><img src="down.png"/>';
+  tdCheckbox.innerHTML = '<input type="checkbox"  onclick="selectRecord(this)"/><br /><br /><img src="down.png"  onclick="showDetail(this)"/>';
   trNode.appendChild(tdCheckbox);
   tdStudent.innerHTML = 'Student ' + curIndex;
   trNode.appendChild(tdStudent);
@@ -60,18 +62,19 @@ function addRecord() {
   trNode.appendChild(tdPercentage);
   tdDelete.innerHTML = '<button id="deleteBTN" onclick="deleteRecord(this)">Delete</button>';
   trNode.appendChild(tdDelete);
+  tdEdit.innerHTML = '<button id="editBTN" onclick="editRecord(this)">Edit</button>';
+  trNode.appendChild(tdEdit);
   // put the tr into table
   table.appendChild(trNode);
 
 
   // create the detailed row
-  let trNodeDetail = document.createElement("tr");
-
+  let trNodeDetail = document.createElement("tr");;
   // insert the detail
   trNodeDetail.innerHTML = detailContent;
   table.appendChild(trNodeDetail);
-  // set the class of detail row 
-  let lastDetail = table.lastElementChild.setAttribute("class", "dropDownTextArea");
+  // not show the detail
+  let lastDetail = table.lastElementChild.setAttribute("style", "display: none;")
 
   alert('Student ' + curIndex + " Record added successfully")
 }
@@ -83,24 +86,51 @@ function addRecord() {
 function selectRecord(checkBox) {
   let selectedRow = checkBox.parentElement.parentElement;
   if (checkBox.checked == true) {
+    // the row is now selected
+    selectedNum ++;
     selectedRow.style.backgroundColor = "yellow";
     selectedRow.lastElementChild.lastElementChild.style.display = "inline";
+    selectedRow.lastElementChild.previousElementSibling.lastElementChild.style.display = "inline";
     document.getElementById("submitBTN").style.backgroundColor = "orange";
   } else {
+    // cancel the selection of the row
+    selectedNum --;
     selectedRow.style.backgroundColor = "white";
     selectedRow.lastElementChild.lastElementChild.style.display = "none";
-    document.getElementById("submitBTN").style.backgroundColor = "gray";
+    selectedRow.lastElementChild.previousElementSibling.lastElementChild.style.display = "none";
+    if (selectedNum == 0) {
+      document.getElementById("submitBTN").style.backgroundColor = "gray";
+    }
   }
 }
 
-function deleteRecord(deleteRow) {
-  let selectedRow = deleteRow.parentElement.parentElement;
-  document.getElementById("myTable").deleteRow(selectedRow.rowIndex);
-  alert(selectedRow.firstElementChild.nextElementSibling.innerHTML + " Record deleted successfully")
-  
+function deleteRecord(rowRef) {
+  let selectedRow = rowRef.parentElement.parentElement;
+  let curRowName = selectedRow.firstElementChild.nextElementSibling.innerHTML;
+  let msg = "You want to delete record of " + curRowName + "?";
+  if (confirm(msg)) {
+    document.getElementById("myTable").deleteRow(selectedRow.rowIndex+1);
+    document.getElementById("myTable").deleteRow(selectedRow.rowIndex);
+    document.getElementById("submitBTN").style.backgroundColor = "gray";
+    alert(curRowName + " Record deleted successfully")
+  }
 }
 
-function showDetail(clickedRow) {
+function showDetail(clickRef) {
+  let detailStatus = clickRef.parentElement.parentElement.nextElementSibling.style.display;
+  if (detailStatus == "none") {
+    clickRef.parentElement.parentElement.nextElementSibling.style.display = "";
+  } else {
+    clickRef.parentElement.parentElement.nextElementSibling.style.display = "none";
+  }
+}
 
-  
+function editRecord(rowRef) {
+  let selectedRow = rowRef.parentElement.parentElement;
+  let curRowName = selectedRow.firstElementChild.nextElementSibling.innerHTML;
+  let content = prompt("Update the " + curRowName, "content here");
+  if (content != null && content != "") {
+    alert (curRowName + " name updated successfully!")
+  }
+
 }
